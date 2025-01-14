dad96ba1949946f58ad98ae2e169e2f7c04bbf6b
dvt-sujay <sujay.singh@gotroo.in>
2025-01-14 19:45:24 +0530

    Add support for a single number

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,7 @@
 class StringCalculator
     def add(numbers)
-      return 0 if numbers.empty?
-    end
+        return 0 if numbers.empty?
+        return numbers.to_i if numbers.match(/^\d+$/)
+    end      
 end
   
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe StringCalculator do
     calculator = StringCalculator.new
     expect(calculator.add("")).to eq(0)
   end
+
+  it "returns the number itself for a single number" do
+    calculator = StringCalculator.new
+    expect(calculator.add("5")).to eq(5)
+  end  
 end
