c0e6a4c3f4af15c9c97c131beeefba0fe3d0783d
dvt-sujay <sujay.singh@gotroo.in>
2025-01-14 19:44:27 +0530

    Add support for empty string, return 0

---
+++ lib/string_calculator.rb
@@ -0,0 +1,6 @@
+class StringCalculator
+    def add(numbers)
+      return 0 if numbers.empty?
+    end
+end
+  
---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,8 @@
+require 'string_calculator'
+
+RSpec.describe StringCalculator do
+  it "returns 0 for an empty string" do
+    calculator = StringCalculator.new
+    expect(calculator.add("")).to eq(0)
+  end
+end
