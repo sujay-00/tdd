a0774ba6ce1ee8fc4caa7b4cd41711d0abd5854c
dvt-sujay <sujay.singh@gotroo.in>
2025-01-14 19:46:22 +0530

    Add support for two numbers separated by a comma

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,7 @@ class StringCalculator
     def add(numbers)
         return 0 if numbers.empty?
         return numbers.to_i if numbers.match(/^\d+$/)
-    end      
+        return numbers.split(",").map(&:to_i).sum
+    end            
 end
   
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -9,5 +9,10 @@ RSpec.describe StringCalculator do
   it "returns the number itself for a single number" do
     calculator = StringCalculator.new
     expect(calculator.add("5")).to eq(5)
+  end
+  
+  it "returns the sum of two numbers" do
+    calculator = StringCalculator.new
+    expect(calculator.add("1,2")).to eq(3)
   end  
 end
