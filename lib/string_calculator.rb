b356c751ab32a7449896e0032ed5a9b70a5b01fd
dvt-sujay <sujay.singh@gotroo.in>
2025-01-14 19:47:57 +0530

    Add support for newlines as delimiters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,8 +1,8 @@
 class StringCalculator
     def add(numbers)
         return 0 if numbers.empty?
-        return numbers.to_i if numbers.match(/^\d+$/)
-        return numbers.split(",").map(&:to_i).sum
-    end                  
+        numbers = numbers.gsub("\n", ",")
+        numbers.split(",").map(&:to_i).sum
+    end                        
 end
   
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -19,5 +19,10 @@ RSpec.describe StringCalculator do
   it "returns the sum of multiple numbers" do
     calculator = StringCalculator.new
     expect(calculator.add("1,2,3,4,5")).to eq(15)
+  end
+  
+  it "handles newlines as delimiters" do
+    calculator = StringCalculator.new
+    expect(calculator.add("1\n2,3")).to eq(6)
   end  
 end
